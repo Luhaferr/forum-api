1f42b0659e3e2573149f174809ce217a0eead2f1
Luiz Ferreira <luhaferr@gmail.com>
2024-11-27 12:37:15 -0300

    listagem e paginacao

--- src/main/java/br/com/forumhub/forum_api/controller/TopicoController.java
+++ src/main/java/br/com/forumhub/forum_api/controller/TopicoController.java
@@ -1,15 +1,16 @@
 package br.com.forumhub.forum_api.controller;
 
 import br.com.forumhub.forum_api.domain.topicos.DadosCriarTopico;
+import br.com.forumhub.forum_api.domain.topicos.DadosListarTopico;
 import br.com.forumhub.forum_api.domain.topicos.TopicosRepository;
 import br.com.forumhub.forum_api.domain.topicos.CriacaoDeTopico;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/topicos")
@@ -25,5 +26,10 @@ public class TopicoController {
     @Transactional
     public void criarTopico(@RequestBody @Valid DadosCriarTopico dados) {
         validador.criar(dados);
+    }
+
+    @GetMapping
+    public Page<DadosListarTopico> listar(@PageableDefault(size = 10, sort = {"dataCriacao"})Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListarTopico::new);
     }
 }
--- src/main/java/br/com/forumhub/forum_api/domain/topicos/DadosListarTopico.java
+++ src/main/java/br/com/forumhub/forum_api/domain/topicos/DadosListarTopico.java
@@ -3,4 +3,7 @@ package br.com.forumhub.forum_api.domain.topicos;
 import java.time.LocalDateTime;
 
 public record DadosListarTopico(String titulo, String mensagem, LocalDateTime dataCriacao, boolean status, String autor, String curso) {
+    public DadosListarTopico(Topico topico) {
+        this(topico.getTitulo(), topico.getMensagem(), topico.getDataCriacao(), topico.isStatus(), topico.getAutor(), topico.getCurso());
+    }
 }
